7762268c4420542757419b461d0bf6c00454e3bb
MarLeo <martin.aurele12@gmail.com>
2017-01-22 14:25:45 +0100

    configuring project....

--- src/main/java/com/hadoop/project/Driver.java
+++ src/main/java/com/hadoop/project/Driver.java
@@ -1,9 +1,20 @@
 package com.hadoop.project;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * Created by marti on 22/01/2017.
  */
 public class Driver {
 
-    private String name;
+    public static void main(String[] args) {
+
+        final Logger LOGGER = LogManager.getLogger(Driver.class);
+        LOGGER.info(String.format("Lauching %s at %s", Driver.class.getSimpleName(), new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(Calendar.getInstance().getTime())));
+
+    }
 }
