6d14f708c8a0004645dc456d25113331f1f7a18e
Martin <martin.aurele12@gmail.com>
2017-02-15 16:37:57 +0100

    Update PairsReducer.java

--- src/main/java/org/hadoop/project/reducer/PairsReducer.java
+++ src/main/java/org/hadoop/project/reducer/PairsReducer.java
@@ -2,20 +2,20 @@ package org.hadoop.project.reducer;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.hadoop.project.model.WordPair;
+import org.hadoop.project.model.TextPair;
 
 import java.io.IOException;
 
 /**
  * Created by marti on 05/02/2017.
  */
-public class PairsReducer extends Reducer<WordPair, IntWritable, WordPair, IntWritable> {
+public class PairsReducer extends Reducer<TextPair, IntWritable, TextPair, IntWritable> {
 
     private IntWritable total = new IntWritable ();
 
 
     @Override
-    protected void reduce(WordPair key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+    protected void reduce(TextPair key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int count = 0;
         for (IntWritable value : values) {
             count += value.get ();
